28c3bb1b3626f4a8b6a4302a12e3f7442178f959
sarahglass321-commits <sarahglass321@hotmail.com>
2026-01-19 16:00:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,16 @@ def tradealert():
         "where": request.args.get("where"),
         "apikey": "c3Ur9P4izmE9Ig"
     }
-    r = requests.get("https://quant.trade-alert.com", params=params, timeout=10)
-    return jsonify(r.json())
+
+    try:
+        r = requests.get("https://quant.trade-alert.com", params=params, timeout=10)
+        try:
+            return jsonify(r.json())
+        except ValueError:
+            return {"error": "Response not valid JSON", "text": r.text}, r.status_code
+    except Exception as e:
+        return {"error": str(e)}, 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
